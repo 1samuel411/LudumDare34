706d0f353667a07f9f7f11c63d333bd391c11032
Alex Moreno <alexmoreno777@gmail.com>
2015-12-28 18:40:36 -0800

    Updated SpawnBullets and FireWeapon for shotgun.

--- LudumDare34/Assets/Scripts/WeaponSystems/WepShotgun.cs
+++ LudumDare34/Assets/Scripts/WeaponSystems/WepShotgun.cs
@@ -1,7 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WepShotgun : BaseWeapon {
+public class WepShotgun : BaseWeapon
+{
+    public float spraySpread = 0;
+    public int sprayAmount = 1;
 
-	
+    protected override void SpawnBullets(float rotation = 0.0f)
+    {
+        for (int i = 0; i < sprayAmount; i++)
+        {
+            rotation = 0;
+            switch (i)
+            {
+                case 0:
+                    rotation = -spraySpread;
+                    break;
+                case 1:
+                    rotation = 0;
+                    break;
+                case 2:
+                    rotation = spraySpread;
+                    break;
+            }
+            base.SpawnBullets(rotation);
+        }
+    }
 }
